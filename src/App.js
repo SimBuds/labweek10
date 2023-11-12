e222d9b1b91a4b42998ee50db55b40e688389d8b
SimBuds <casey-hsu@outlook.com>
2023-11-12 18:51:52 -0500

    First Push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    email: '',
+    fullName: '',
+    address: '',
+    city: '',
+    province: '',
+    postalCode: ''
+  });
+
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedData(formData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Data Entry Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Full Name:</label>
+          <input type="text" name="fullName" value={formData.fullName} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Address:</label>
+          <input type="text" name="address" value={formData.address} onChange={handleChange} />
+        </div>
+        <div>
+          <label>City:</label>
+          <input type="text" name="city" value={formData.city} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Province:</label>
+          <input type="text" name="province" value={formData.province} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Postal Code:</label>
+          <input type="text" name="postalCode" value={formData.postalCode} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+
+      {submittedData && (
+        <div className="display-box">
+          <p>Email: {submittedData.email}</p>
+          <p>Full Name: {submittedData.fullName}</p>
+          <p>Address: {submittedData.address}</p>
+          <p>City: {submittedData.city}</p>
+          <p>Province: {submittedData.province}</p>
+          <p>Postal Code: {submittedData.postalCode}</p>
+        </div>
+      )}
     </div>
   );
 }
